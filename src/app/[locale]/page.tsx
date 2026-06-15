351a576f55ad7652ee69dea2a3e650f36a613e05
bastosjt <bastosjamet@gmail.com>
2026-06-15 11:52:38 +0200

    fix: translations for i18n

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,6 @@
-import { useTranslations } from "next-intl";
+import { getTranslations } from "next-intl/server";
 
 export default async function Page() {
-  const t = await useTranslations("Home");
+  const t = await getTranslations("Home");
   return <div>{t("title")}</div>;
 }
